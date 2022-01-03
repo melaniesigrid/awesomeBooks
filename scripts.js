6e99701d23aa4f71c385ee726c4de68c1ab27487
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-01-03 16:42:16 -0500

    Changed remove function to filter by title and author
    - Change Add button type
    - Create checkrepetition function to add just one instance of book
    - Create alert to tell when a book is repeated

--- scripts.js
+++ scripts.js
@@ -5,17 +5,16 @@ function Book(title, author) {
   this.author = author;
 }
 
-const removeBook = (index) => {
-  library = library.filter((element, ind) => ind !== index);
-}
+const removeBook = (title, author) => {
+  library = library.filter((book) => book.title !== title || book.author !== author);
+};
 
 const booksContainer = document.querySelector('.book-container');
 
-function renderBooks() {
-  library.forEach((book) => {
-    displayBook(book);
-  })
-}
+const saveDataLocally = (library) => {
+  const stringifiedLibrary = JSON.stringify(library);
+  localStorage.setItem('library', stringifiedLibrary);
+};
 
 function displayBook(book) {
   const bookCard = document.createElement('div');
@@ -23,45 +22,69 @@ function displayBook(book) {
   const cardAuthor = document.createElement('p');
   const removeButton = document.createElement('button');
 
-  bookCard.classList.add('book-Card')
+  bookCard.classList.add('book-Card');
 
   cardTitle.textContent = book.title;
   cardAuthor.textContent = book.author;
   removeButton.textContent = 'Remove';
-
-  removeButton.addEventListener('click', () => {
-    removeBook(index);
-    saveDataLocally(library);
-  });
 
   bookCard.appendChild(cardTitle);
   bookCard.appendChild(cardAuthor);
   bookCard.appendChild(removeButton);
 
   booksContainer.appendChild(bookCard);
+
+  removeButton.addEventListener('click', () => {
+    removeBook(book.title, book.author);
+    saveDataLocally(library);
+    booksContainer.removeChild(bookCard);
+  });
 }
 
-const saveDataLocally = (library) => {
-  const stringifiedLibrary = JSON.stringify(library);
-  localStorage.setItem('library', stringifiedLibrary);
+function renderBooks() {
+  library.forEach((book) => {
+    displayBook(book);
+  });
+}
+
+const inputTitle = document.querySelector('#title');
+const inputAuthor = document.querySelector('#author');
+
+function checkRepetition(book) {
+  for (let i = 0; i < library.length; i += 1) {
+    const currentBook = library[i];
+    if (currentBook.title.toLowerCase() === book.title.toLowerCase()
+    && currentBook.author.toLowerCase() === book.author.toLowerCase()) {
+      alert('Book already added');
+      return false;
+    }
+  }
+  return true;
 }
 
 function addBookToLibrary() {
-  const inputTitle = document.querySelector('#title').value;
-  const inputAuthor = document.querySelector('#author').value;
-  const book = new Book(inputTitle, inputAuthor);
-  library.push(book);
-  saveDataLocally(library);
-  displayBook(book);
+  const inputTitleValue = inputTitle.value;
+  const inputAuthorValue = inputAuthor.value;
+  const book = new Book(inputTitleValue, inputAuthorValue);
+  if (checkRepetition(book)) {
+    library.push(book);
+    saveDataLocally(library);
+    displayBook(book);
+  }
 }
 
-const addButton = document.querySelector('.add');
-addButton.addEventListener('click', () => addBookToLibrary());
+const addForm = document.querySelector('form');
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  inputTitle.value = '';
+  inputAuthor.value = '';
+});
 
 window.onload = () => {
   if (localStorage.getItem('library') !== null) {
-  let myLibrary = JSON.parse(localStorage.getItem('library'));
-  library = myLibrary;
-  } 
+    const myLibrary = JSON.parse(localStorage.getItem('library'));
+    library = myLibrary;
+  }
   renderBooks();
-}
+};
